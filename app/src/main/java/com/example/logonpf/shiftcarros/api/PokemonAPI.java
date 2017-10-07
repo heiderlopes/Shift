47a6ee066b9d615a9ea7c78688ec875f26803ff4
Heider Lopes <heidertreinamentos@gmail.com>
2017-10-07 17:51:41 -0300

    Servico para pesquisa por id do pokemon

--- app/src/main/java/com/example/logonpf/shiftcarros/api/PokemonAPI.java
+++ app/src/main/java/com/example/logonpf/shiftcarros/api/PokemonAPI.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by logonpf on 07/10/2017.
@@ -16,5 +17,8 @@ public interface PokemonAPI {
 
     @GET("api/v2/pokemon")
     Call<PokemonResponse> getPokemons();
+
+    @GET("api/v2/pokemon/{numeroPokemon}")
+    Call<Pokemon> getPokemon(@Path(value = "numeroPokemon") int numeroPokemon);
 
 }
